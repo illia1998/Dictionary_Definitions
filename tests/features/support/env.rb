b4f3e84d7e4949efb7d654a5dd9c31760ca47074
Illia Ferents <illiaferents@gmail.com>
2023-03-08 20:40:05 +0200

    add basic allure reporting

--- tests/features/support/env.rb
+++ tests/features/support/env.rb
@@ -1,6 +1,16 @@
 require_relative '../../../bin/find_term_html'
 require 'active_support/testing/stream'
 require_relative 'helper_methods'
+require 'allure-cucumber'
+
+AllureCucumber.configure do |config|
+  config.results_directory = 'report/allure-results'
+  config.clean_results_directory = true
+end
+
+at_exit do
+  system('allure generate report/allure-results --clean')
+end
 
 World(ActiveSupport::Testing::Stream, HelperMethods)
 
