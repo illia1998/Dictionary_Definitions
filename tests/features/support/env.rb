3ba45845f6db6f73d4ab2733f6a0215aa0c07e2a
Illia Ferents <95219692+IlliaFerents@users.noreply.github.com>
2023-04-19 23:24:43 +0300

    Update env.rb

--- tests/features/support/env.rb
+++ tests/features/support/env.rb
@@ -6,7 +6,7 @@ require_relative 'helper_methods'
 require 'allure-cucumber'
 
 AllureCucumber.configure do |config|
-  config.results_directory = 'allure-results'
+  config.results_directory = 'report/allure-results'
   config.clean_results_directory = true # set 'false' to see runs trend
 end
 
