17f553fd667a2ef8e9c43107f9815950d54520f6
Illia Ferents <95219692+IlliaFerents@users.noreply.github.com>
2023-05-04 22:12:36 +0300

    Update calculate_pass_rate.groovy

--- calculate_pass_rate.groovy
+++ calculate_pass_rate.groovy
@@ -2,14 +2,15 @@ def calculatePassRate() {
     sh 'cd tests && grep -Eo ":[0-9]+" results/final_failures.txt | wc -l > rerun-failures-count'
     def failedScenarios = readFile 'tests/rerun-failures-count'.trim()
     def totalScenarios = sh(returnStdout: true, script: "cd tests && ruby count_scenarios.rb ${params.FEATURES}")
-    def passRate = (failedScenarios.toFloat() / totalScenarios.toFloat()) * 100
+    def passedScenarios = (totalScenarios.toFloat() - failedScenarios.toFloat()).toInteger()
+    def passRate = (passedScenarios / totalScenarios.toFloat()) * 100
     
     return [
         failedScenarios: failedScenarios.trim(),
         totalScenarios: totalScenarios.trim(),
+        passedScenarios:  passedScenarios,
         passRate: passRate.round(2)
     ]
 }
 
 calculatePassRate()
- 
