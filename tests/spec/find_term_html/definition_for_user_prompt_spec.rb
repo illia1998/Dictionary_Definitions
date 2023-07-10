0e9efb270202d8a79896323849f57db7e016c84a
Illia Ferents <illiaferents@gmail.com>
2023-07-10 14:33:18 +0300

    refactor definition_for_user_prompt_spec.rb

--- tests/spec/find_term_html/definition_for_user_prompt_spec.rb
+++ tests/spec/find_term_html/definition_for_user_prompt_spec.rb
@@ -1,18 +1,17 @@
 RSpec.describe DictionaryDefinitionsParse do
   context '#definition_for', :user_prompt do
-    context 'when retrieving definitions for a word' do
-      include_examples 'valid user prompt', 'micromanagement', 'prompts for user input if term is not provided'
-      include_examples 'valid user prompt', 'full-scale', 'prompts for user input if term is not provided'
+    context 'when retrieving definitions with valid user input' do
+      include_examples 'valid user prompt', :sense, 'prompts for user input if term is not provided'
+      include_examples 'valid user prompt', :idiom, 'prompts for user input if term is not provided'
+    end
+
+    context 'when retrieving definitions with invalid user input' do
+      include_examples 'invalid user prompt', :sense, 'raises error for invalid input'
+      include_examples 'invalid user prompt', :origin, 'raises error for invalid input'
     end
   end
 end
 
-RSpec.describe DictionaryDefinitionsParse do
-  context '#definition_for', :user_prompt do
-    context 'when retrieving definitions for a word' do
-      include_examples 'invalid user prompt', ' ', 'raises InvalidURLError for empty search', InvalidURLError
-      include_examples 'invalid user prompt', nil, 'raises NotFoundError for nil', NotFoundError
-    end
-  end
-end
+
+
 
