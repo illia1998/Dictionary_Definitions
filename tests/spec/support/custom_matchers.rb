d314b862dc47176b632a3c3cafa339cf32c53b00
Illia Ferents <illiaferents@gmail.com>
2023-07-10 21:38:31 +0300

    create CustomMatchers module

--- tests/spec/support/custom_matchers.rb
+++ tests/spec/support/custom_matchers.rb
@@ -1,6 +1,8 @@
-RSpec::Matchers.define :be_a_non_empty_array do
-  match do |actual|
-    expect(actual).to be_an(Array)
-    expect(actual).not_to be_empty
+module CustomMatchers
+  RSpec::Matchers.define :be_a_non_empty_array do
+    match do |actual|
+      expect(actual).to be_an(Array)
+      expect(actual).not_to be_empty
+    end
   end
 end
